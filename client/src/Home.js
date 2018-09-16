9908e50284c3bfda09709d6f5cb66361e0de1698
Kien Nguyen <kc97ble@gmail.com>
2018-09-16 19:17:12 +0800

    Extract function

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 
+function Form(props) {
+  const {
+    caption,
+    placeholder
+  } = props
+
+  return (
+    <form action="" className="index_form">
+      <input type="text" placeholder={placeholder} className="index_formtext col-10"/>
+      <button type="submit" className="index_formbutton col-2">{caption}</button>
+    </form>
+  )
+}
+
 class Home extends Component {
   render() {
     return (
       <div>
-        <div class="index_maincontent">
-          <div class="index_transparentbox">
-            <div class="index_searchcontainer padding_1">
-              <div class="index_tablecontainer">
-                <form action="" class="index_form">
-                  <input type="text" placeholder="Find a Professor by Name" class="index_formtext col-10"/>
-                  <button tyoe="submit" class="index_formbutton col-2">GO</button>
-                </form>
+        <div className="index_maincontent">
+          <div className="index_transparentbox">
+            <div className="index_searchcontainer padding_1">
+              <div className="index_tablecontainer">
+                <Form placeholder="Find a Professor by Name" caption="GO"/>
               </div>
             </div>
           </div>
