e9c5ad295753dfc8aa98e687e0320a8a8cd6bb5c
Kien Nguyen <kc97ble@gmail.com>
2018-09-23 13:34:07 +0800

    Refactor Profs.js

--- client/src/Profs.js
+++ client/src/Profs.js
@@ -1,7 +1,45 @@
-import React, { Component } from 'react';
-import Prof from './Prof.js';
+import React, { Component } from "react";
+import Prof from "./Prof.js";
 
 class Profs extends Component {
+
+  renderProf(props) {
+    const { name, faculty, rating } = props;
+    return (
+      <div className="profs_box lightgrey_background">
+        <div className="col-4 profs_boxphoto no_padding">
+          <img src="/img/anonymous.jpg"/>
+        </div>
+        <div className="col-8 profs_boxdetails">
+          <div className="profs_boxdetailsname font_uppermedium_content">
+            {name}
+          </div>
+          <div className="profs_boxdetailsdept font_lowermedium_content">
+            {faculty}
+          </div>
+          <div className="profs_boxdetailsrating font_lowermedium_content">
+            Rating:&nbsp;
+            <span className="profs_ratingstar">
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <span className="profs_ratingstars" id="profs_ratingstars_1">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </span>
+            </span>
+            <span className="profs_ratingval"> ({rating.toFixed(2)})</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     const {
       match
@@ -14,37 +52,16 @@ class Profs extends Component {
         </div>
 
         <div className="profs_boxlist">
-          <div className="profs_box lightgrey_background">
-            <div className="col-4 profs_boxphoto no_padding">
-              <img src="/img/anonymous.jpg"/>
-            </div>
-            <div className="col-8 profs_boxdetails">
-              <div className="profs_boxdetailsname font_uppermedium_content">
-                Foo Bar
-              </div>
-              <div className="profs_boxdetailsdept font_lowermedium_content">
-                Department of Computer Science
-              </div>
-              <div className="profs_boxdetailsrating font_lowermedium_content">
-                Rating:&nbsp;
-                <span className="profs_ratingstar">
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <i class="far fa-star"></i>
-                  <span className="profs_ratingstars" id="profs_ratingstars_1">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                  </span> 
-                </span>
-                <span className="profs_ratingval"> (4.5)</span>
-              </div>
-            </div>
-          </div>
+          {this.renderProf({
+            name: "Steven Halim",
+            faculty: "Department of Computer Science",
+            rating: 4.5
+          })}
+          {this.renderProf({
+            name: "Colin Tan",
+            faculty: "Department of Computer Science",
+            rating: 3.5
+          })}
         </div>
 
       </div>
