d391b825d5f688b6677817a242e915a6240dec15
Samuel Henry Kurniawan <samuelhenrykurniawan@yahoo.com>
2018-09-26 21:11:29 +0800

    refactor css completed

--- client/src/Rate.js
+++ client/src/Rate.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { getProf, getAllTags } from "./api.js";
+import "./Rate.css";
+
 
 class Rate extends Component {
 
@@ -26,12 +28,12 @@ class Rate extends Component {
   renderProfName() {
     return (
       <div className="form-group">
-        <label htmlFor="rateProfName" className="font_lowermedium_content">
+        <label htmlFor="rate-form__name" className="font-size--m">
           {"Professor's Name"}
         </label>
         <input
           type="text"
-          id="rateProfName"
+          id="rate-form__name"
           className="form-control"
           value={this.state.prof.first_name + " " + this.state.prof.last_name}
           readOnly={true}
@@ -43,8 +45,8 @@ class Rate extends Component {
   renderModuleInput() {
     return (
       <div className="form-group">
-        <label htmlFor="rateProfModule" className="font_lowermedium_content">Module</label>
-        <select className="form-control" id="rateProfModule">
+        <label htmlFor="rate-form__module" className="font-size--m">Module</label>
+        <select className="form-control" id="rate-form__module">
           <option>Select a module...</option>
           {
             this.state.prof.modules.map(module => {
@@ -64,8 +66,8 @@ class Rate extends Component {
     const gradeList = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "D", "F"];
     return(
       <div className="form-group">
-        <label htmlFor="rateProfGrade" className="font_lowermedium_content">Grades Obtained</label>
-        <select className="form-control" id="rateProfGrade">
+        <label htmlFor="rate-prof__grade" className="font-size--m">Grades Obtained</label>
+        <select className="form-control" id="rate-form__grade">
           <option>Select your grade...</option>
           {
             gradeList.map(grade =>
@@ -80,24 +82,24 @@ class Rate extends Component {
   renderRatingInput() {
     return (
       <div className="form-group">
-        <label htmlFor="rateProfRating" className="font_lowermedium_content">{"Prof's Rating"}</label>
-        <div className="rate_starlist">
-          <span className="rate_stars">
+        <label htmlFor="rate-form__rating" className="font-size--m">{"Prof's Rating"}</label>
+        <div className="rate-form__stars-container">
+          <span className="rate-value">
             {
               [1, 2, 3, 4, 5].map(rate =>
-                <i className="fas darker-star fa-star fa-3x rate_star" value={rate}></i>
+                <i className="fas fa-star fa-3x rate-form__star star--dark" value={rate}></i>
               )
             }
-            <span className="rate_stars_2" id="rateProfStars">
+            <span className="rate-value--coloured" id="rate-form__stars--coloured">
               {
                 [1, 2, 3, 4, 5].map(rate =>
-                  <i className="fas fa-star fa-3x rate_star" value={rate}></i>
+                  <i className="fas fa-star fa-3x rate-form__star--coloured star--bright" value={rate}></i>
                 )
               }
             </span>
           </span>
         </div>
-        <select className="form-control" id="rateProfRating">
+        <select className="form-control" id="rate--form__rating">
           {
             [1, 2, 3, 4, 5].map(rate =>
               <option value={rate}></option>
@@ -111,29 +113,29 @@ class Rate extends Component {
   renderDifficultyInput() {
     return (
       <div className="form-group">
-        <label htmlFor="rateProfRating" className="font_lowermedium_content">Difficulty Level</label>
-        <div className="rate_starlist">
-          <span className="rate_fires">
-            <i className="fab fa-hotjar fa-3x rate_fire" value={1}></i>
-            <i className="fab fa-hotjar fa-3x rate_fire" value={2}></i>
-            <i className="fab fa-hotjar fa-3x rate_fire" value={3}></i>
-            <i className="fab fa-hotjar fa-3x rate_fire" value={4}></i>
-            <i className="fab fa-hotjar fa-3x rate_fire" value={5}></i>
-            <span className="rate_fires_2" id="rateProfFires">
-              <i className="fab fa-hotjar fa-3x rate_fire" value={1}></i>
-              <i className="fab fa-hotjar fa-3x rate_fire" value={2}></i>
-              <i className="fab fa-hotjar fa-3x rate_fire" value={3}></i>
-              <i className="fab fa-hotjar fa-3x rate_fire" value={4}></i>
-              <i className="fab fa-hotjar fa-3x rate_fire" value={5}></i>
+        <label htmlFor="rate-form__difficulty" className="font-size--m">{"Difficulty Level"}</label>
+        <div className="rate-form__fires-container">
+          <span className="rate-value">
+            {
+              [1, 2, 3, 4, 5].map(rate =>
+                <i className="fab fa-hotjar fa-3x rate-form__fire fire--dark" value={rate}></i>
+              )
+            }
+            <span className="rate-value--coloured" id="rate-form__fires--coloured">
+              {
+                [1, 2, 3, 4, 5].map(rate =>
+                  <i className="fab fa-hotjar fa-3x rate-form__fire--coloured fire--bright" value={rate}></i>
+                )
+              }
             </span>
           </span>
         </div>
-        <select className="form-control" id="rateProfDifficulty">
-          <option value={1}></option>
-          <option value={2}></option>
-          <option value={3}></option>
-          <option value={4}></option>
-          <option value={5}></option>
+        <select className="form-control" id="rate--form__rating">
+          {
+            [1, 2, 3, 4, 5].map(rate =>
+              <option value={rate}></option>
+            )
+          }
         </select>
       </div>
     );
@@ -142,18 +144,18 @@ class Rate extends Component {
   renderTagInput() {
     return (
       <div className="form-group">
-        <label className="font_lowermedium_content">Choose Tags</label>
-        <div className="form-check rate_tagscheckbox" id="rateProfTags">
+        <label className="font-size--m">Choose Tags</label>
+        <div className="form-check rate-form__checkbox" id="rate-form__tags">
           <input className="form-check-input" type="checkbox" value="" id="rateTags_1"/>
           <input className="form-check-input" type="checkbox" value="" id="rateTags_2"/>
           <input className="form-check-input" type="checkbox" value="" id="rateTags_3"/>
           <input className="form-check-input" type="checkbox" value="" id="rateTags_4"/>
           <input className="form-check-input" type="checkbox" value="" id="rateTags_5"/>
         </div>
-        <div className="rate_taglist">
+        <div className="rate-form__taglist">
           {
             this.state.tags.map(tag =>
-              <span className="rate_tag">{tag.definition}</span>
+              <span className="rate-form__tag">{tag.definition}</span>
             )
           }
         </div>
@@ -164,8 +166,8 @@ class Rate extends Component {
   renderCommentInput() {
     return (
       <div className="form-group">
-        <label htmlFor="rateProfComment" className="font_lowermedium_content">Further Comments</label>
-        <textarea type="textarea" rows={6} id="rateProfComment" className="form-control" placeholder="Write down all your comments..." />
+        <label htmlFor="rate-form__comment" className="font-size--m">Further Comments</label>
+        <textarea type="textarea" rows={6} id="rate-form__comment" className="form-control" placeholder="Write down all your comments..." />
       </div>
     );
   }
@@ -176,7 +178,7 @@ class Rate extends Component {
 
   renderSubmitButton() {
     return (
-      <button type="submit" class="btn rate_submit">
+      <button type="submit" class="btn rate-form__submit">
         Submit
       </button>
     );
@@ -184,8 +186,8 @@ class Rate extends Component {
 
   render() {
     return (
-      <div className="padding_1">
-        <div className="rate font_uppermedium_content">
+      <div className="page">
+        <div className="rate-form font-size--xl">
           Rate Your Prof!
         </div>
 
