f002b05301855168e5f94a790dc0b860d1aecaa1
ChrisMaxheart <samuelhenrykurniawan@yahoo.com>
2018-09-28 23:50:33 +0800

    minor fix

--- client/src/About.js
+++ client/src/About.js
@@ -15,7 +15,7 @@ class About extends Component {
 
         <div className="about__text font-size--m">
           Do you feel annoyed with the fact that only your Professors can give you feedback and grade you and not the vice versa?<br/><br/>
-          Fret not, with RateYourProfs, you can start grade and giving feedback to your professors.<br/><br/>
+          Fret not, with Rate Your Profs, you can start grade and giving feedback to your professors.<br/><br/>
           Your feedback could help your Professors to improve themselves.<br/>
           Your feedback could also be useful for your juniors that considering to take modules taught by that particular prof.<br/><br/>
           So, what do you wait for? Sign up and start giving feedback to your profs!
